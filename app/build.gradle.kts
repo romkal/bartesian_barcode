604a2ae647558a9726bc95c1637cd374106cc166
Roman Kalukiewicz <roman.kalukiewicz@gmail.com>
2025-01-02 12:49:26 -0800

    Version bump and water amount change to start from 0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "us.romkal.barcode"
     minSdk = 24
     targetSdk = 35
-    versionCode = 5
-    versionName = "0.05"
+    versionCode = 6
+    versionName = "0.06"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
