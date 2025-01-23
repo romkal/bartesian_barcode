4d1248b26506bf7bc285bc0fb8b4467661eb0993
Roman Kalukiewicz <roman.kalukiewicz@gmail.com>
2025-01-23 09:18:43 -0800

    Changed the name to mention Bartesian.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "us.romkal.barcode"
     minSdk = 24
     targetSdk = 35
-    versionCode = 14
-    versionName = "0.14"
+    versionCode = 15
+    versionName = "0.15"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
